e4fca28b9940c7dfa2d2c006de147bf1d5237df4
zhaojl131415 <zhaojl131415@163.com>
2018-04-21 23:25:58 +0800

    hystrix

---
+++ movie-ribbon-with-hystrix/src/main/java/com/demo/movie/entity/User.java
@@ -0,0 +1,49 @@
+package com.demo.movie.entity;
+
+public class User {
+    private Integer id;
+
+    private String username;
+
+    private String name;
+
+    private String age;
+
+    public User() {
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAge() {
+        return age;
+    }
+
+    public void setAge(String age) {
+        this.age = age;
+    }
+
+
+
+}
